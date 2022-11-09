0405b838261a5aee876b235d70cc18acf8fc5d05
Hackerl <490021209@qq.com>
2022-11-09 18:24:37 +0800

    Add#stringify ipv6

--- ebpf/src/stringify.h
+++ ebpf/src/stringify.h
@@ -7,9 +7,31 @@
 #include <bpf/bpf_helpers.h>
 #include <sys/param.h>
 
+#define HEX_MAX_COUNT 10
 #define SLICE_MAX_COUNT 10
 #define INT64_STR_MAX_LENGTH 19
 #define UINT64_STR_MAX_LENGTH 20
+
+static __always_inline int hexlify(const unsigned char *bytes, size_t count, char *buffer, size_t size) {
+    if (count * 2 >= size)
+        return 0;
+
+#pragma unroll
+    for (int i = 0; i < HEX_MAX_COUNT; i++) {
+        if (i >= count)
+            break;
+
+        unsigned char b[2] = {
+                ((bytes[i] & 0xf0) >> 4),
+                bytes[i] & 0x0f
+        };
+
+        buffer[i * 2] = b[0] + (b[0] < 10 ? '0' : 'a' - 10);
+        buffer[i * 2 + 1] = b[1] + (b[1] < 10 ? '0' : 'a' - 10);
+    }
+
+    return (int) count * 2;
+}
 
 static __always_inline int stringify_go_uint64(go_uint64 num, char *buffer, size_t size) {
     size_t length = 0;
@@ -129,16 +151,13 @@ static __always_inline int stringify_string_slice(slice *s, char *buffer, size_t
     return (int) length;
 }
 
-static __always_inline int stringify_ip(slice *ip, char *buffer, size_t size) {
-    if (ip->count != 4)
-        return 0;
-
+static __always_inline int stringify_ipv4(slice *ip, char *buffer, size_t size) {
     unsigned char bytes[4];
 
     if (bpf_probe_read_user(&bytes, sizeof(bytes), ip->data) < 0)
         return -1;
 
-    size_t length = 0;
+    volatile size_t length = 0;
 
 #pragma unroll
     for (int i = 0; i < 4 * 2 - 1; i++) {
@@ -163,6 +182,48 @@ static __always_inline int stringify_ip(slice *ip, char *buffer, size_t size) {
     buffer[BOUND(length, ARG_LENGTH)] = 0;
 
     return (int) length;
+}
+
+static __always_inline int stringify_ipv6(slice *ip, char *buffer, size_t size) {
+    unsigned char bytes[16];
+
+    if (bpf_probe_read_user(&bytes, sizeof(bytes), ip->data) < 0)
+        return -1;
+
+    size_t length = 0;
+
+#pragma unroll
+    for (int i = 0; i < 8 * 2 - 1; i++) {
+        if (length >= size - 1) {
+            buffer[0] = 0;
+            return 0;
+        }
+
+        if (i % 2) {
+            buffer[BOUND(length++, ARG_LENGTH)] = ':';
+            continue;
+        }
+
+        int n = hexlify(bytes + i, 2, buffer + BOUND(length, ARG_LENGTH), size - BOUND(length, ARG_LENGTH));
+
+        if (n < 0)
+            return -1;
+
+        length += n;
+    }
+
+    buffer[BOUND(length, ARG_LENGTH)] = 0;
+
+    return (int) length;
+}
+
+static __always_inline int stringify_ip(slice *ip, char *buffer, size_t size) {
+    if (ip->count == 4)
+        return stringify_ipv4(ip, buffer, size);
+    else if (ip->count == 16)
+        return stringify_ipv6(ip, buffer, size);
+
+    return 0;
 }
 
 static __always_inline int stringify_tcp_address(tcp_address *address, char *buffer, size_t size) {
